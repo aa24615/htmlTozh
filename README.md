1cf08b9f8db062ced900752508025a49ade5f24a
读心印 <839024615@qq.com>
2020-12-24 09:32:00 +0800

    test

--- README.md
+++ README.md
@@ -2,3 +2,46 @@
 
 > HTML翻译工具
 
+
+### 安装
+
+下载
+
+> git clone git@github.com:aa24615/htmlTozh.git
+
+如果网络较慢,可使用国内gitee
+
+> git clone git@gitee.com:flash127/htmlToZh.git
+
+安装依懒
+
+> npm i
+
+
+### 准备
+
+- 将你的手册或需要翻译的文件 放在根目录下 html/项目名 中
+- 例如 nodejs文档 放在 html/nodedoc/
+
+
+### 运行
+
+- 运行 `node run.js 项目名` 例如 `node run.js nodedoc` 等待翻译
+
+### 输出
+
+- 如果文件较多,时间可能较长,几分种到几小时不等,翻译完成之后,会生成相对应该的 _zh_ch 目录
+
+### 缓存
+
+- 无需担心翻译中断,需要重新翻译,内置翻译缓存,缓存文件在根目录的`cache`下,如果强制更新译文,请删除`cache`目录即可
+
+
+
+### 联系我们
+
+- QQ群: [295968682](https://qm.qq.com/cgi-bin/qm/qr?k=rgnEr60hWRuAkEj9nLzGlzvYmBfQsrWW&jump_from=webapi)
+
+- 博客: [http://blog.php127.com](http://blog.php127.com)
+
+- 更多开源项目: [https://github.com/aa24615](https://github.com/aa24615)
--- run.js
+++ run.js
@@ -1,74 +1,35 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
 const path = require('path');
 const parse = require('./lib/parse');
 const {translate} = require('./lib/translate');
-
-const fse = require('fs-extra');
-
-let dir = path.join(process.cwd(), 'html/getcomposer.org');
+const tools = require('./lib/tools');
 
 
+let dirname = process.argv[2];
 
+if(!dirname){
+    console.error("请输入翻译文件夹 ");
+    console.log('npm run [dirname]');
+    console.log("-----------------------------------");
+    console.log("请将项目文件放在本目录的html中");
+    console.log("示例:");
+    console.log("html/nodejs");
+    console.log("html/php8");
+    return;
+}
 
-//复盖目标目录
+let dir = path.join(process.cwd(), 'html', dirname);
+
+//目标目录
 let outDir = dir + '_zh_cn';
-copyFolder(dir, outDir);
+tools.copyFolder(dir, outDir);
 
-let list = getList(dir, outDir);
+let list = tools.getList(outDir);
+
 //生成json
-
 list.forEach(file => {
     parse(file);
 });
+
 translate(list);
-
-//同步拷贝目录
-function copyFolder(copiedPath, resultPath) {
-
-    if (fs.existsSync(resultPath)) {
-        fse.removeSync(resultPath)
-    }
-
-    fs.mkdirSync(resultPath);
-
-    if (fs.existsSync(copiedPath)) {
-        fse.copySync(copiedPath, resultPath)
-    } else {
-        console.log('这个目录不存在:', copiedPath);
-    }
-}
-
-//获取所有html或htm
-function getList(dir = '') {
-    if (dir == "") {
-        console.log('请输入目录');
-        return;
-    }
-
-    let files = fs.readdirSync(dir);
-    let list = [];
-    files.forEach(async (item, index) => {
-        let fPath = path.join(dir, item);
-
-        console.log(dir, item);
-
-        let stat = fs.statSync(fPath);
-        if (stat.isDirectory() === true) {
-
-            let sublist = getList(fPath);
-            sublist.forEach(file => {
-                list.push(file);
-            })
-        }
-
-        if (stat.isFile() === true) {
-            if (fPath.slice(-4) == 'html' || fPath.slice(-3) == 'htm') {
-                list.push(fPath);
-            }
-        }
-    });
-
-    return list;
-}
